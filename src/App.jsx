01a09f1670b53e735fcaed1d95a19d6ebdf2fea9
Nicholas Chan <nnchan777@gmail.com>
2025-03-25 18:39:08 -0700

    home page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import './App.css'
-import CodeEditor from './components/Editor/EditorOld.jsx'
 import EditorPage from './editor/page.jsx';
-import Home from './components/Home/Home.jsx';
-import SignIn from './components/SignIn/signin.jsx';
-import SignUp from './components/SignIn/SignUp.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BaseLayout from './layout/page.jsx';
 import Dashboard from './dashboard/page.jsx';
+import Home from './home/page.jsx';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/editor/:id" element={<EditorPage />} />
-        <Route path="/editor" element={<CodeEditor />} />
         <Route element={<BaseLayout />}>
-          <Route path="/" element={''} />
+          <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Route>
       </Routes>
